88611837c2b2a2865218fa03fd109ce7a1b673cd
Sunil.Jacob <sunil1405@gmail.com>
2019-06-07 11:50:11 +0530

    Cleared shellcheck validation

--- main.sh
+++ main.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-BLUE='\033[0;36m'
-NC='\033[0m' # No Color
 
+# shellcheck source=somefile
 source spell_checker.sh
 
 echo -e "\e[44mStarting main script...$NC"
--- spell_checker.sh
+++ spell_checker.sh
@@ -1,3 +1,8 @@
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;36m'
+NC='\033[0m' # No Color
+
 initiate_spell_check() {
   echo "Spellcheck module started..."
 }
